8212dbe4c360c0a6d7fea136b648636541ab2e2b
David <david.sato64@gmail.com>
2018-08-23 23:49:56 +0200

    Updated WalletIntegration to show simple logic use w/state Provider

--- 03_add-frontend-react/src/components/controls/WalletIntegration.jsx
+++ 03_add-frontend-react/src/components/controls/WalletIntegration.jsx
@@ -5,8 +5,6 @@ import Providers from '../../api/providers'
 import { connect } from '../Provider'
 import { getAPI } from '../../api'
 import { getAppContracts } from '../../api/Contracts';
-import { getWeb3API } from '../../api/ProviderWeb3';
-
 
 class WalletIntegration extends React.Component {
   state = {
@@ -16,23 +14,33 @@ class WalletIntegration extends React.Component {
     web3: undefined,
   }
 
-  async componentWillMount() {
-    console.log('​WalletIntegration -> asynccomponentWillMount -> ', Providers);
+  componentWillMount() {
+    const { registerProviders } = this.props
+    // returns [ Provider{}, ... ]
+    const providersArray = Object.values(Providers)
+    // register each providerObject into state
+    return providersArray.forEach(provider => { registerProviders(providersArray) })
   }
 
   onChange = async providerInfo => {
-    const { setActiveProvider } = this.props
+    const { grabUserState, setActiveProvider } = this.props
 
     try {
       this.setState({ initialising: true, error: undefined })
+      const chosenProvider = Providers[providerInfo]
       // initialize providers and return specific Web3 instances
-      const web3 = await Providers[providerInfo].initialize()
+      const web3 = await chosenProvider.initialize()
 
+      // Save activeProvider to State
+      setActiveProvider(providerInfo)
+      // Save web3 provider + notify state locally
       this.setState({ web3, activeProviderSet: true })
 
       // interface with contracts & connect entire DX API
       await getAppContracts()
       await getAPI()
+      
+      await grabUserState(chosenProvider)
 
       return this.setState({ initialising: false })
     } catch (error) {
@@ -47,20 +55,15 @@ class WalletIntegration extends React.Component {
         <h1>Please select a wallet</h1>
         <div className={!this.state.initialising ? 'lightBlue' : ''}>
         {Object.keys(Providers).map((provider, i) => {
-            const providerInfo = Providers[provider].providerName || provider
-            return (
+          const providerInfo = Providers[provider].providerName || provider
+          return (
             <div
-                key={i}
-                onClick={() => this.onChange(provider)}
+              key={i}
+              onClick={() => this.onChange(provider)}
             >
-                <h4>{providerInfo}</h4>
-                <br/>
-                {/*<img
-                src={provider2SVG(providerInfo)}
-                style={{ minHeight: 26, maxHeight: 45, marginTop: -6 }}
-                />*/}
+              <h4 className="providerChoice">{`${i+1}. ${providerInfo}`}</h4>
             </div>
-            )
+          )
         })}
         </div>
         {this.state.error && <h3>{this.state.error.message}</h3>}
@@ -70,13 +73,31 @@ class WalletIntegration extends React.Component {
 
   render() {
     const { initialising, activeProviderSet } = this.state,
-      { /* activeProvider,  */children } = this.props
-    return (/* activeProvider &&  */activeProviderSet) && !initialising ? children : this.walletSelector()
+      { activeProvider, children } = this.props
+    return (activeProvider && activeProviderSet) && !initialising ? children : this.walletSelector()
   }
 }
 
-/* const mapState = ({ blockchain: { activeProvider } }) => ({
+const mapState = ({
+  // state properties
+  state: { 
+    providerData: { activeProvider, network }, 
+    user: { account, balance } 
+  },
+  // dispatchers
+  grabUserState,
+  registerProviders,
+  setActiveProvider,
+}) => ({
+  // state properties
   activeProvider,
-}) */
+  network,
+  account,
+  balance,
+  // dispatchers
+  grabUserState,
+  registerProviders,
+  setActiveProvider,
+})
 
-export default connect()(WalletIntegration)
+export default connect(mapState)(WalletIntegration)
