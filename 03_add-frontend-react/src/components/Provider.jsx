dcfe3808f8ef598a1879f864346b1233ed07a2bf
David <david.sato64@gmail.com>
2018-08-23 23:49:30 +0200

    Simplified Provider.jsx to use as example/bootstrap

--- 03_add-frontend-react/src/components/Provider.jsx
+++ 03_add-frontend-react/src/components/Provider.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
+import { getAccount, getNetwork, getBalance } from '../api/providers'
 
 const defaultState = {
-  account: 'CONNECTION ERROR',
-  network: 'NETWORK NOT SUPPORTED',
+  user: {
+    account: 'CONNECTION ERROR',
+    balance: undefined,
+  },
+  providerData: {
+    activeProvider: undefined,
+    network: 'NETWORK NOT SUPPORTED',
+    providers: [],
+  }
 }
 
 const { Provider, Consumer } = React.createContext(defaultState)
 
 const setToContext = new WeakMap()
-const memoizedContextValue = ({ state }) => {
+const memoizedContextValue = ({ state, grabUserState, registerProviders, setActiveProvider }) => {
   if (setToContext.has(state)) return setToContext.get(state)
 
-  const contextValue = { state }
+  const contextValue = { state, grabUserState, registerProviders, setActiveProvider }
   setToContext.set(state, contextValue)
   return contextValue
 }
@@ -21,15 +29,31 @@ class AppProvider extends React.Component {
     ...defaultState
   }
 
+  grabUserState = async provider => {
+    const [account, network] = await Promise.all([
+      getAccount(provider),
+      getNetwork(provider),
+    ])
+    const balance = await getBalance(provider, account)
+    return this.setState({ user: { account, balance }, providerData: { network, ...this.state.providerData } })
+  }
+
+  registerProviders = provider => this.setState({ providerData: { providers: [...provider, ...this.state.providerData.providers] } })
+  setActiveProvider = providerName => this.setState({ providerData: { activeProvider: providerName, ...this.state.providerData,  } })
+
   render() {
     const { loading } = this.state
     return (
       <Provider value={memoizedContextValue(this)}>
-        {process.env.NODE_ENV === 'production' && (
-          <pre style={{ position: 'fixed', top: '5vh', left: 0, zIndex: 99, backgroundColor: '#e0e0ecc9' }}>
-            {JSON.stringify(this.state, null, 2)}
-          </pre>
-        )}
+        {
+         /*
+          * Printing state - runs into circular JSON stringify errors
+          process.env.NODE_ENV === 'development' && (
+            <pre style={{ position: 'fixed', bottom: 0, left: 0, zIndex: 99, backgroundColor: '#e0e0ecc9' }}>
+              {JSON.stringify(this.state, null, 2)}
+            </pre>
+          ) 
+         */}
         {loading ? null : this.props.children}
       </Provider>
     )
