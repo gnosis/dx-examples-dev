caa28b5d4a8fad4fb7d058906ea363bd37217dfe
David <david.sato64@gmail.com>
2018-08-30 15:58:26 +0200

    added some state props in Home to display

--- 03_add-frontend-react/src/components/display/Home.jsx
+++ 03_add-frontend-react/src/components/display/Home.jsx
@@ -1,27 +1,46 @@
 import React from 'react'
-import { connect } from '../Provider'
+import { connect } from '../StateProvider'
 
-const Home = ({ account, balance: userBalance, network }) => (
-    <div>
-        <h3>Network: {network}</h3>
-        <br/>
-        <h3>Account: {account}</h3>
-        <h3>Balance: {userBalance}</h3>
-    </div>
+const Home = ({ account, balance: userBalance, network, tokens, tokenFRT, tokenOWL, priceFeed }) => (
+  <div>
+    <h1>Provider state:</h1>
+
+    <h3>Network: {network}</h3>
+    <h3>Account: {account}</h3>
+    <h3>Balance: {userBalance}</h3>
+
+    <br /><br />
+
+    <h1>Misc. DutchX state:</h1>
+
+    <h3>DutchX Token Balances:</h3>
+    <ol>
+      {Object.keys(tokens).length && Object.keys(tokens).map(tokenAddr =>
+        <li key={tokenAddr}>Symbol: {tokens[tokenAddr].symbol} \\ Balance: {tokens[tokenAddr].balance}</li>)}
+    </ol>
+    {tokenFRT && <h3>FRT Token Address: <a target="_blank" rel="noopener noreferrer" href={`https://${network === 'RINKEBY' ? 'rinkeby.' : ''}etherscan.io/token/${tokenFRT.address}`}>{tokenFRT.address}</a></h3>}
+    {tokenOWL && <h3>OWL Token Address: <a target="_blank" rel="noopener noreferrer" href={`https://${network === 'RINKEBY' ? 'rinkeby.' : ''}etherscan.io/token/${tokenOWL.address}`}>{tokenOWL.address}</a></h3>}
+    {priceFeed && <h3>Current Price Feed Address: <a target="_blank" rel="noopener noreferrer" href={`https://${network === 'RINKEBY' ? 'rinkeby.' : ''}etherscan.io/address/${priceFeed}`}>{priceFeed}</a></h3>}
+  </div>
 )
 
-const mapProps = ({ 
-    state: {
-        user: {
-            account,
-            balance,
-        },
-        providerData: { network },
-    } 
+const mapProps = ({
+  state: {
+    USER: {
+      account,
+      balance,
+    },
+    PROVIDER: { network },
+    DX: { tokens, tokenFRT, tokenOWL, priceFeed },
+  },
 }) => ({
-    account,
-    balance,
-    network,
+  account,
+  balance,
+  network,
+  tokens,
+  tokenFRT,
+  tokenOWL,
+  priceFeed,
 })
 
 export default connect(mapProps)(Home)
